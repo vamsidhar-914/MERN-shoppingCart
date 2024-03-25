522da24d3c42541ea0fd0911c55384ed5e870a2e
vamsidhar-914 <vamsimay12345@gmail.com>
2024-03-25 10:49:11 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>TS amazon</header>
+      <main></main>
+      <footer>All rigths reserved</footer>
+    </div>
+  );
+}
+
+export default App;
